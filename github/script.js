21e9a2c2c8a22edf61dd1b81c0a24469f2f673e9
Noman <mdnoman70499@gmail.com>
2023-08-02 01:13:37 +0530

    add some more

--- github/script.js
+++ github/script.js
@@ -2,3 +2,5 @@
 // hey man how are you doing what is the process of your career and how's going right now
 // this is something that i'm going to do with javascript ffile I hope you all are fine with this
 // How is going in github in your area of field
+
+// this is somthing that i want to do one more time
