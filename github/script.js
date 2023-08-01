218e0a6ec8194bb45d4dbc18082e9381666a825d
Noman <mdnoman70499@gmail.com>
2023-08-02 01:11:06 +0530

    commit 4th time

--- github/script.js
+++ github/script.js
@@ -1,3 +1,4 @@
 // this is javascript file but i don't know what to write I will write somthing in it very soon
 // hey man how are you doing what is the process of your career and how's going right now
 // this is something that i'm going to do with javascript ffile I hope you all are fine with this
+// How is going in github in your area of field
