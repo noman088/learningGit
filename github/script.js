644658e5932b91feae326d5a0da12bb5627190cf
Noman <mdnoman70499@gmail.com>
2023-08-02 01:06:57 +0530

    somthing  changed in js

--- github/script.js
+++ github/script.js
@@ -1,1 +1,2 @@
 // this is javascript file but i don't know what to write I will write somthing in it very soon
+// hey man how are you doing what is the process of your career and how's going right now
